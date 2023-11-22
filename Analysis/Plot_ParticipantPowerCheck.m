3c91abac6c5def8170399d1240182aba26d72678
Sophia Snipes <snipeso@users.noreply.github.com>
2023-11-22 10:55:45 +0100

    plot gamma peak

--- Analysis/Plot_ParticipantPowerCheck.m
+++ Analysis/Plot_ParticipantPowerCheck.m
@@ -12,6 +12,7 @@ Refresh = true;
 PlotProps = Parameters.PlotProps.Manuscript;
 
 nFreqs = 1025;
+nChans = 109;
 Folder = 'window8s_allt/';
 
 CacheDir = Paths.Cache;
@@ -26,12 +27,15 @@ if ~exist(ResultsFolder,'dir')
     mkdir(ResultsFolder)
 end
 
+GammaRange = [25, 35];
+
 
 
 %%% gather data
 if Refresh || ~exist(fullfile(CacheDir, CacheName), 'file')
     EEGPowerSource = fullfile(Paths.AnalyzedData, 'EEG', 'Power', Folder);
     PowerSpectra = nan(Parameters.Participants, 4, 2, nFreqs);
+    GammaTopographies = nan(Parameters.Participants, nChans);
 
     for DatasetCell = Datasets
         Dataset = DatasetCell{1};
@@ -59,7 +63,21 @@ if Refresh || ~exist(fullfile(CacheDir, CacheName), 'file')
 
                 PrevSession = squeeze(PowerSpectra(ParticipantID, TaskIdx, HourID, :))';
                 AllSessions = cat(1, PrevSession, mean(Power, 1, 'omitnan'));
-                PowerSpectra(ParticipantID, TaskIdx, HourID, :) = mean(AllSessions, 1, 'omitnan');
+                Spectrum = mean(AllSessions, 1, 'omitnan');
+                SpectrumSmooth =  smooth_frequencies(Spectrum, Freqs, 2);
+                PowerSpectra(ParticipantID, TaskIdx, HourID, :) = SpectrumSmooth;
+
+
+                % gather topography of peak frequency
+                if TaskIdx == 1 && HourID == 1
+                    PeakFreq = find_periodic_peak(SpectrumSmooth, Freqs, GammaRange);
+                    if ~isempty(PeakFreq)
+                        BumpIndex = ismember(Freqs, PeakFreq);
+                        GammaTopographies(ParticipantID, :) = Power(:, BumpIndex);
+                    end
+                end
+
+
                 disp(['Finished ' Filename])
             end
         end
@@ -70,17 +88,24 @@ else
 end
 
 
-            PowerSpectraSmooth = smooth_frequencies(PowerSpectra, Freqs, 2);
 
 
-%% plot data
+%% plot spectra
 
 close all
 
+plotGamma = true;
 FigureDimentions = [5 8];
 
 Colors = chART.color_picker([4, 2]);
 
+if plotGamma
+    Range = [25 40];
+    FigLabel = 'GAMMAPower_';
+else
+    Range = [1 40];
+    FigLabel = 'AllPower_';
+end
 Idx = 1;
 figure('Units','normalized','OuterPosition',[0 0 1 1])
 for ParticipantIdx = 1:Parameters.Participants
@@ -89,32 +114,55 @@ for ParticipantIdx = 1:Parameters.Participants
     hold on
     for TaskIdx = 1:4
         for HourIdx = 1:2
-            Data = squeeze(PowerSpectraSmooth(ParticipantIdx, TaskIdx, HourIdx, :));
+            Data = squeeze(PowerSpectra(ParticipantIdx, TaskIdx, HourIdx, :));
 
             plot(Freqs, Data, 'LineWidth', 1.5, 'Color', squeeze(Colors(HourIdx, :, TaskIdx)));
             xlabel('Frequency (Hz)')
             ylabel('Power')
-            set(gca, 'XScale', 'log', 'YScale', 'log', 'xlim', [25 40])
+            set(gca, 'XScale', 'log', 'YScale', 'log', 'xlim', Range)
             title(ParticipantIdx)
         end
     end
 
 
-Idx = Idx+1;
-if Idx > FigureDimentions(1)*FigureDimentions(2)
-    chART.save_figure(['GAMMAPower_',num2str(ParticipantIdx)], ResultsFolder, PlotProps)
-    Idx = 1;
-    figure('Units','normalized','OuterPosition',[0 0 1 1])
-elseif ParticipantIdx == Parameters.Participants
-      chART.save_figure(['GAMMAAllPower_',num2str(ParticipantIdx)], ResultsFolder, PlotProps)
+    Idx = Idx+1;
+    if Idx > FigureDimentions(1)*FigureDimentions(2)
+        chART.save_figure([FigLabel,num2str(ParticipantIdx)], ResultsFolder, PlotProps)
+        Idx = 1;
+        figure('Units','normalized','OuterPosition',[0 0 1 1])
+    elseif ParticipantIdx == Parameters.Participants
+        chART.save_figure([FigLabel, num2str(ParticipantIdx)], ResultsFolder, PlotProps)
+    end
 end
+
+
+%% plot gamma topographies
+
+
+FigLabel = 'GammaTopo';
+Idx = 1;
+figure('Units','normalized','OuterPosition',[0 0 1 1])
+CLims = 'minmax';
+for ParticipantIdx = 1:Parameters.Participants
+
+    Data = GammaTopographies(ParticipantIdx, :);
+    if ~all(isnan(Data))
+        subplot(FigureDimentions(1), FigureDimentions(2), Idx)
+        hold on
+        chART.plot.eeglab_topoplot(Data, Chanlocs, [], CLims, '', 'Linear', PlotProps)
+        colorbar off
+        title(ParticipantIdx)
+    end
+
+    Idx = Idx+1;
+    if Idx > FigureDimentions(1)*FigureDimentions(2)
+        chART.save_figure([FigLabel,num2str(ParticipantIdx)], ResultsFolder, PlotProps)
+        Idx = 1;
+        figure('Units','normalized','OuterPosition',[0 0 1 1])
+    elseif ParticipantIdx == Parameters.Participants
+        chART.save_figure([FigLabel, num2str(ParticipantIdx)], ResultsFolder, PlotProps)
+    end
 end
-
-
-
-
-
-
 
 
 
