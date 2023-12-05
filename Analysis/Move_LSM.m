3e069fd53de486e5fc729ac039d8e3b892d35186
Sophia Snipes <snipeso@users.noreply.github.com>
2023-12-05 12:53:06 +0100

    consider copying lsm

---
+++ Analysis/Move_LSM.m
@@ -0,0 +1,17 @@
+
+% new filename: Participant_Dataset_Session_hour_task_repeat.mat
+
+Source = 'E:\Data\Preprocessed\Clean\Waves';
+Tasks = {'Fixation', 'Oddball', 'Standing'};
+Participants = {'P01', 'P02', 'P03', 'P04', 'P05', 'P06', 'P07', 'P08', ...
+    'P09', 'P10', 'P11', 'P12', 'P13', 'P14', 'P15', 'P16', 'P17', 'P18', 'P19'};
+Sessions = {'BaselinePre', 'BaselinePost'};
+Hours = {'eve', 'mor'};
+Dataset = 'LSM';
+
+
+for Task = Tasks
+
+
+
+end
