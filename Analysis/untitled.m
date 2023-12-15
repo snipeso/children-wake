bbc12cff1ae97eb03c963143fd3d495893a810b4
Sophia Snipes <snipeso@users.noreply.github.com>
2023-12-15 17:24:02 +0100

    match patients and controls

--- Analysis/untitled.m
+++ Analysis/untitled.m
@@ -21,7 +21,7 @@ TempMetadata = Metadata(Metadata.Age >=Ages(1) & Metadata.Age<=Ages(2), :);
 OvernightMetadata = overnight_changes(TempMetadata);
 Groups = {'HC', 'ADHD'};
 CLims = [-1 1];
-PlotProps.PlotN = true;
+PlotProps.Stats.PlotN = true;
 
 figure('Units','normalized','Position', [0 0 .35 .15*nMeasures])
 for MeasuresIdx = 1:nMeasures
@@ -29,23 +29,28 @@ for MeasuresIdx = 1:nMeasures
     Topographies = BurstInformationTopography.(Measures{MeasuresIdx});
 
     % overnight changes
-    for GroupIdx = 1:numel(Groups)
-        EveningIndexes = OvernightMetadata.EveningIndexes(strcmp(OvernightMetadata.Group, Groups{GroupIdx}));
-        MorningIndexes = OvernightMetadata.MorningIndexes(strcmp(OvernightMetadata.Group, Groups{GroupIdx}));
-        Evening = Topographies(EveningIndexes, :);
-        Morning = Topographies(MorningIndexes, :);
+    for HourIdx = 1:numel(Hours)
+        
+        HourMetadata = TempMetadata(strcmp(TempMetadata.Hour, Hours{HourIdx}), :);
+        
+        [MetadataPatients, MetadataControls] = match_participants(HourMetadata, strcmp(HourMetadata.Group, 'ADHD'));
 
-        Evening = average_by_column(TempMetadata, EveningIndexes, Evening, 'Participant');
-        Morning = average_by_column(TempMetadata, MorningIndexes, Morning, 'Participant');
+        ADHD = Topographies(MetadataPatients.Index, :);
+        Control = Topographies(MetadataControls.Index, :);
 
-        chART.sub_plot([], [nMeasures, 2], [MeasuresIdx, GroupIdx], [], false, '', PlotProps);
-        plot_topography_difference(Evening, Morning, Chanlocs, CLims, Parameters.Stats, PlotProps)
+        ADHD = average_by_column(MetadataPatients, MetadataPatients.Index, ADHD, 'Participant');
+        Control = average_by_column(MetadataControls, MetadataControls.Index, Control, 'Participant');
+
+        chART.sub_plot([], [nMeasures, 2], [MeasuresIdx, HourIdx], [], false, '', PlotProps);
+        StatsParameters = Parameters.Stats;
+        StatsParameters.Unpaired = true;
+        plot_topography_difference(Control, ADHD, Chanlocs, CLims, StatsParameters, PlotProps)
         colorbar off
 
 
         X = get(gca, 'XLim');
         Y = get(gca, 'YLim');
-        if GroupIdx ==1
+        if HourIdx ==1
             text(X(1)-diff(X)*.15, Y(1)+diff(Y)*.5, Measures{MeasuresIdx}, ...
                 'FontSize', PlotProps.Text.TitleSize, 'FontName', PlotProps.Text.FontName, ...
                 'FontWeight', 'Bold', 'HorizontalAlignment', 'Center', 'Rotation', 90);
--- functions/stats/unpaired_ttest.m
+++ functions/stats/unpaired_ttest.m
@@ -1,33 +1,32 @@
 function Stats = unpaired_ttest(Data1, Data2, StatsP)
 
-   [~, p, CI, stats] = ttest2(Data1, Data2);
-    [Sig, crit_p, ~, adj_P] = fdr_bh(p, StatsP.Alpha, StatsP.ttest.dep); % NOTE: dep is good for ERPs, since data can be negatively correlated as well
-    t_values = stats.tstat';
+[~, p, CI, stats] = ttest2(Data1, Data2);
+[Sig, crit_p, ~, adj_P] = fdr_bh(p, StatsP.Alpha, StatsP.ttest.dep); % NOTE: dep is good for ERPs, since data can be negatively correlated as well
+t_values = stats.tstat';
 
-    Stats.t = t_values(:);
-    Stats.p = p(:);
-    Stats.p_fdr = adj_P;
-    Stats.crit_p = crit_p;
-    Stats.sig = Sig(:);
-    Stats.df = stats.df(:);
-    Stats.N = totN(Data1, Data2);
-    Stats.CI = CI';
- 
-    Stats.mean1 = mean(Data1, 1, 'omitnan')';
-    Stats.std1 = std(Data1, 0, 1, 'omitnan')';
-    Stats.mean2 = mean(Data2, 1, 'omitnan')';
-    Stats.std2 = std(Data2, 0, 1, 'omitnan')';
+Stats.t = t_values(:);
+Stats.p = p(:);
+Stats.p_fdr = adj_P;
+Stats.crit_p = crit_p;
+Stats.sig = Sig(:);
+Stats.df = stats.df(:);
+Stats.N = totN(Data1, Data2);
+Stats.CI = CI';
+
+Stats.mean1 = mean(Data1, 1, 'omitnan')';
+Stats.std1 = std(Data1, 0, 1, 'omitnan')';
+Stats.mean2 = mean(Data2, 1, 'omitnan')';
+Stats.std2 = std(Data2, 0, 1, 'omitnan')';
 
 
-    
 
-    stats =  unpaired_hedgesG(Data1, Data2, StatsP);
-    Stats.(StatsP.Paired.ES) = stats.(StatsP.Paired.ES);
-    Stats.([StatsP.Paired.ES, 'CI']) = stats.([StatsP.Paired.ES, 'CI']);
+
+stats =  unpaired_hedgesG(Data1, Data2, StatsP);
+Stats.(StatsP.Paired.ES) = stats.(StatsP.Paired.ES);
+Stats.([StatsP.Paired.ES, 'CI']) = stats.([StatsP.Paired.ES, 'CI']);
 
 end
 
-    function N = totN(Data1, Data2)
-        warning('wrongN')
-N = 1;
+function N = totN(Data1, Data2)
+N = size(Data1, 1);
 end
