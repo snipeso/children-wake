7f6cd6eba733d7932f28629ba83ae04011deacb0
Sophia Snipes <snipeso@users.noreply.github.com>
2023-12-23 14:32:39 +0100

    handle if fit doesnt work without crashing everything

--- functions/eeg/fooof_spectrum.m
+++ functions/eeg/fooof_spectrum.m
@@ -4,9 +4,17 @@ simple_fooof_fittingRange = [2 35];
 
 Power = smooth_frequencies(Power, Frequencies, 2);
 
+try
 FooofModel = fooof(Frequencies, Power, simple_fooof_fittingRange, struct(), true);
 FooofFrequencies = FooofModel.freqs;
 Intercept = FooofModel.aperiodic_params(1);
 Slope = FooofModel.aperiodic_params(2);
 
 WhitenedPower = FooofModel.power_spectrum-FooofModel.ap_fit;
+catch
+    warning('couldnt fit fooof')
+    Slope = nan;
+    Intercept = nan;
+    WhitenedPower = nan(1, numel(Frequencies));
+    FooofFrequencies = Frequencies;
+end
