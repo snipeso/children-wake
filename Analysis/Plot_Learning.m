c2278e4fc6e87d3a60242cc7c0bc7b9e90235d8d
Sophia Snipes <snipeso@users.noreply.github.com>
2023-12-28 11:59:04 +0100

    direct comparison rot vs base

--- Analysis/Plot_Learning.m
+++ Analysis/Plot_Learning.m
@@ -125,49 +125,110 @@ for ConditionIdx = 1:numel(Conditions)
 end
 
 
-
-%% fixation vs gonogo task
+%%  difference of difference
 
 CLims = [-1.5 1.5];
 
 Measures = {'Amplitude', 'Quantity', 'Slope', 'Intercept', 'Power', 'PeriodicPower'};
 nMeasures = numel(Measures);
+    % MetadataTemp = Metadata(strcmp(Metadata.Hour, 'mor') & strcmp(Metadata.Condition, Conditions{ConditionIdx}), :);
+    % MetadataTemp = Metadata(strcmp(Metadata.Condition, Conditions{ConditionIdx}), :);
 
-MetadataTemp = Metadata(strcmp(Metadata.Hour, 'eve'), :);
-MetadataTemp = Metadata;
-Metadata1 = pair_recordings(MetadataTemp, 'Task', {'3Fixation', '4Fixation'});
-Metadata2 = Metadata1;
-Metadata2.Index = Metadata1.IndexesCategory2;
+    Metadata1 = pair_recordings(Metadata, 'Task', {'1Oddball', '3Oddball'});
+    Oddball1Index = Metadata1.IndexesCategory1;
+    Oddball3Index = Metadata1.IndexesCategory2;
 
-figure('Units','centimeters','OuterPosition',[0 0 25 30])
-for MeasureIdx = 1:nMeasures
-    Topographies = BurstInformationTopography.(Measures{MeasureIdx});
-    for AgeIdx = 1:nAges
-        Indexes = ismember(Metadata1.AgeGroups, string(AgeIdx));
+        Metadata2 = pair_recordings(Metadata1, 'Condition', {'base', 'rotation'});
 
-        Category1 = average_by_column(Metadata1, Topographies, 'Participant', Indexes);
-        Category2 = average_by_column(Metadata2, Topographies, 'Participant', Indexes);
+    Oddball1BaseIndex = Metadata2.IndexesCategory1; % 1oddball base associated with both a 3oddball, and a 1oddball rot
+    Oddball3BaseIndex = Metadata1.IndexesCategory2(ismember(Metadata1.IndexesCategory1, Oddball1BaseIndex));
+    Oddball1RotIndex = Metadata2.IndexesCategory2;
+    Oddball3RotIndex = Metadata1.IndexesCategory2(ismember(Metadata1.IndexesCategory1, Oddball1RotIndex));
 
 
-        chART.sub_plot([], [nMeasures, nAges+1], [MeasureIdx, AgeIdx], [], false, '', TopoPlotProps);
-        plot_topography_difference(Category1, Category2, Chanlocs, CLims, Parameters.Stats, TopoPlotProps) %
-        colorbar off
+    figure('Units','centimeters','OuterPosition',[0 0 15 30])
+    for MeasureIdx = 1:nMeasures
+        Topographies = BurstInformationTopography.(Measures{MeasureIdx});
+                    Base = Topographies(Oddball3BaseIndex, :)-Topographies(Oddball1BaseIndex, :);
+            Rotation = Topographies(Oddball3RotIndex, :)-Topographies(Oddball1RotIndex, :);
 
-        if MeasureIdx == 1
-            title([num2str(Ages(AgeIdx, 1)),'-' num2str(Ages(AgeIdx, 2))])
-        end
+        for AgeIdx = 1:nAges
+            Indexes = ismember(Metadata2.AgeGroups, string(AgeIdx));
 
-        if AgeIdx ==2
-            X = get(gca, 'XLim');
-            Y = get(gca, 'YLim');
-            text(X(1)-diff(X)*.15, Y(1)+diff(Y)*.5, Measures{MeasureIdx}, ...
-                'FontSize', TopoPlotProps.Text.TitleSize, 'FontName', TopoPlotProps.Text.FontName, ...
-                'FontWeight', 'Bold', 'HorizontalAlignment', 'Center', 'Rotation', 90);
+            Category1 = average_by_column(Metadata2(Indexes, :), Base(Indexes, :), 'Participant', []);
+            Category2 = average_by_column(Metadata2(Indexes, :), Rotation(Indexes, :), 'Participant', []);
+
+            chART.sub_plot([], [nMeasures, nAges+1], [MeasureIdx, AgeIdx], [], false, '', TopoPlotProps);
+            plot_topography_difference(Category1, Category2, Chanlocs, CLims, Parameters.Stats, TopoPlotProps) %
+            colorbar off
+
+            if MeasureIdx == 1
+                title([num2str(Ages(AgeIdx, 1)),'-' num2str(Ages(AgeIdx, 2))])
+            end
+
+            if AgeIdx ==1
+                X = get(gca, 'XLim');
+                Y = get(gca, 'YLim');
+                text(X(1)-diff(X)*.15, Y(1)+diff(Y)*.5, Measures{MeasureIdx}, ...
+                    'FontSize', TopoPlotProps.Text.TitleSize, 'FontName', TopoPlotProps.Text.FontName, ...
+                    'FontWeight', 'Bold', 'HorizontalAlignment', 'Center', 'Rotation', 90);
+            end
         end
     end
-end
 
-chART.sub_plot([], [nMeasures, nAges+1], [MeasureIdx, AgeIdx+1], [nMeasures, 1], false, '', TopoPlotProps);
-chART.plot.pretty_colorbar('Divergent', CLims, "Cohen's d", TopoPlotProps)
+    chART.sub_plot([], [nMeasures, nAges+1], [MeasureIdx, AgeIdx+1], [nMeasures, 1], false, '', TopoPlotProps);
+    chART.plot.pretty_colorbar('Divergent', CLims, "Cohen's d", TopoPlotProps)
 
-chART.save_figure('GopherChange', ResultsFolder, TopoPlotProps)
+    chART.save_figure('OddballRotvsBase', ResultsFolder, TopoPlotProps)
+
+
+
+
+
+
+
+%% fixation vs gonogo task
+% 
+% CLims = [-1.5 1.5];
+% 
+% Measures = {'Amplitude', 'Quantity', 'Slope', 'Intercept', 'Power', 'PeriodicPower'};
+% nMeasures = numel(Measures);
+% 
+% MetadataTemp = Metadata(strcmp(Metadata.Hour, 'eve'), :);
+% MetadataTemp = Metadata;
+% Metadata1 = pair_recordings(MetadataTemp, 'Task', {'3Fixation', '4Fixation'});
+% Metadata2 = Metadata1;
+% Metadata2.Index = Metadata1.IndexesCategory2;
+% 
+% figure('Units','centimeters','OuterPosition',[0 0 25 30])
+% for MeasureIdx = 1:nMeasures
+%     Topographies = BurstInformationTopography.(Measures{MeasureIdx});
+%     for AgeIdx = 1:nAges
+%         Indexes = ismember(Metadata1.AgeGroups, string(AgeIdx));
+% 
+%         Category1 = average_by_column(Metadata1, Topographies, 'Participant', Indexes);
+%         Category2 = average_by_column(Metadata2, Topographies, 'Participant', Indexes);
+% 
+% 
+%         chART.sub_plot([], [nMeasures, nAges+1], [MeasureIdx, AgeIdx], [], false, '', TopoPlotProps);
+%         plot_topography_difference(Category1, Category2, Chanlocs, CLims, Parameters.Stats, TopoPlotProps) %
+%         colorbar off
+% 
+%         if MeasureIdx == 1
+%             title([num2str(Ages(AgeIdx, 1)),'-' num2str(Ages(AgeIdx, 2))])
+%         end
+% 
+%         if AgeIdx ==2
+%             X = get(gca, 'XLim');
+%             Y = get(gca, 'YLim');
+%             text(X(1)-diff(X)*.15, Y(1)+diff(Y)*.5, Measures{MeasureIdx}, ...
+%                 'FontSize', TopoPlotProps.Text.TitleSize, 'FontName', TopoPlotProps.Text.FontName, ...
+%                 'FontWeight', 'Bold', 'HorizontalAlignment', 'Center', 'Rotation', 90);
+%         end
+%     end
+% end
+% 
+% chART.sub_plot([], [nMeasures, nAges+1], [MeasureIdx, AgeIdx+1], [nMeasures, 1], false, '', TopoPlotProps);
+% chART.plot.pretty_colorbar('Divergent', CLims, "Cohen's d", TopoPlotProps)
+% 
+% chART.save_figure('GopherChange', ResultsFolder, TopoPlotProps)
