ba90f4392a65a3e97c524c92a3c182ebd0dbeae4
Sophia Snipes <snipeso@users.noreply.github.com>
2024-05-31 15:05:12 +0200

    rename adhd

--- Analysis/Figure8_ADHD.m
+++ Analysis/Figure8_ADHD.m
@@ -33,7 +33,7 @@ CacheName = 'AllBursts.mat';
 
 %% make model basic values
 %%% load data
-load(fullfile(CacheDir, CacheName), 'Metadata', 'BurstInformationTopographyBands', ...
+load(fullfile(CacheDir, CacheName), 'Metadata', ...
     'BurstInformationTopography', 'Chanlocs')
 Metadata = basic_metadata_cleanup(Metadata, {'Tasks', Tasks});
 
@@ -60,7 +60,7 @@ end
 
 
 
-%% Effect of ADHD on outcome measures
+%% Effect of ADHD on outcome measures (Figure 8)
 
 close all
 Coefficient = 'Group_2';
@@ -93,57 +93,3 @@ for MeasureIdx = 1:nMeasures
 end
 
 chART.save_figure('ADHDTopography', ResultsFolder, PlotProps)
-
-
-
-%% make model overnight change values
-
-load(fullfile(CacheDir, CacheName), 'Metadata', 'BurstInformationTopographyBands', ...
-    'BurstInformationTopography', 'Chanlocs')
-Metadata = basic_metadata_cleanup(Metadata);
-
-
-MetadataStat = pair_recordings(Metadata, 'Hour', {'eve', 'mor'});
-
-MetadataStat = make_categorical(MetadataStat, 'Task', Tasks);
-MetadataStat.Participant = categorical(MetadataStat.Participant);
-MetadataStat = make_categorical(MetadataStat, 'Group', {'HC', 'ADHD'});
-MetadataStat.Data = nan(size(MetadataStat, 1), 1);
-
-formula = 'Overnight ~ Age + Task + Group + (1|Participant) + (1|Participant:SessionUnique)';
-
-OvernightModels = cell([nMeasures, nChannels]);
-for MeasureIdx = 1:nMeasures
-    for ChannelIdx = 1:nChannels
-        MetadataTemp = MetadataStat;
-        Evening = BurstInformationTopography.(Measures{MeasureIdx})(MetadataTemp.IndexesCategory1, ChannelIdx);
-        Morning = BurstInformationTopography.(Measures{MeasureIdx})(MetadataTemp.IndexesCategory2, ChannelIdx);
-        MetadataTemp.Overnight = Morning-Evening;
-        OvernightModels{MeasureIdx, ChannelIdx} = fitlme(MetadataTemp, formula);
-    end
-    disp(['Finished ', Measures{MeasureIdx}])
-end
-
-
-
-%% Overnight changes
-
-close all
-CLims = [-3 3];
-Coefficient = 'Group_2';
-ColorParameter = 'Estimate';
-
-Grid = [1, nMeasures];
-
-PlotProps = Parameters.PlotProps.Manuscript;
-
-figure('Units','centimeters','OuterPosition',[0 0 30 8])
-for MeasureIdx = 1:nMeasures
-    chART.sub_plot([], Grid, [1, MeasureIdx], [], false, '', PlotProps);
-
-    mixed_model_topography(squeeze(OvernightModels(MeasureIdx, :)), ...
-        ColorParameter, Coefficient, Chanlocs, [], PlotProps)
-    title(Measures{MeasureIdx})
-end
-
-chART.save_figure('ADHDTopographyOvernight', ResultsFolder, PlotProps)
