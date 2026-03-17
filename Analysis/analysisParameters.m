f075bd5cadbb1e9aed26e16e63fabe501658b0b2
Sophia Snipes <snipeso@users.noreply.github.com>
2026-03-17 15:04:10 +0100

    mac path

--- Analysis/analysisParameters.m
+++ Analysis/analysisParameters.m
@@ -72,6 +72,8 @@ elseif exist( 'X:\Data\Raw', 'dir')
 %     Core = 'E:\AllWake';
 elseif exist( 'D:\Data\AllWake\', 'dir')
     Core = 'D:\Data\AllWake';
+elseif exist('/Users/sophia/Documents/Data/WakeData', 'dir')
+    Core = '/Users/sophia/Documents/Data/WakeData';
 else
     error('no data disk!')
     % Core = 'E:\'
