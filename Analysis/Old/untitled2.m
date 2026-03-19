54463a0d5e9b07af3a5abc95be8e3e59603fed25
Sophia Snipes <snipeso@users.noreply.github.com>
2026-03-19 15:32:35 +0100

    remove old scripts

--- Analysis/Old/untitled2.m
+++
@@ -1,8 +0,0 @@
-load(fullfile(Paths.Metadata, 'SleepScoring.mat'), 'Metadata')
-ScoringMetadata = Metadata;
-
-Datasets = Parameters.Datasets;
-
-ScoringMetadata(~ismember(ScoringMetadata.Dataset, Datasets), :) = [];
-
-save(fullfile(Paths.Metadata, 'SleepScoringForWake.mat'), 'ScoringMetadata')
